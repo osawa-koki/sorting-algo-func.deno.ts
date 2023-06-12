51a009213004e04ed28b5a8439201f824ac2a89f
osawa-koki <github@osawa.dev>
2023-06-13 01:12:40 +0900

    Revert "関数を作成した。"
    This reverts commit 251f52d116bdcd504567c4a47dea91cb522c7129.

--- tests/add.test.ts
+++ tests/add.test.ts
@@ -1,7 +1,7 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import add from "../src/add.ts";
 
-Deno.test("Testing `add` function.", () => {
+Deno.test("sum() adds given numbers", () => {
   const actual = add(1, 2);
   const expected = 3;
   assertEquals(actual, expected);
